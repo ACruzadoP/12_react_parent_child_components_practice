4137aa9e902f9de3af6855b744195a227064acf9
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:54:19 +0200

    Tomando snapshot de un ejercicio terminado

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header () {
+    return (
+        <header>
+            <nav>
+                <h1>Esto es un Navbar</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainContent () {
+    return (
+        <main>
+            <h1>Esto será mi Main Content</h1>
+        </main>
+    )
+}
+
+export default MainContent
